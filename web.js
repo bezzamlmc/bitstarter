ebeef9dd4b53948093a856a960004e269817fd67
bezzamlmc <bezzam@aol.com>
2013-07-10 07:48:16 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var infile = "index.html";
+
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString());
+  response.send(fs.readFileSync(infile).toString());
 });
 
 var port = process.env.PORT || 5000;
